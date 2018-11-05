41d3907bc408bd480bb56e12a5b3d28688eea483
Nathan Klapstein <nklapste@ualberta.ca>
2018-11-05 14:14:15 -0700

    adding list bookings module

---
+++ mini_project_1/list_bookings.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""List bookings
+
+The member should be able to list all bookings on rides s/he offers.
+"""
+
+from mini_project_1.common import ShellArgumentParser
+
+
+def get_list_bookings_parser() -> ShellArgumentParser:
+    parser = ShellArgumentParser(
+        add_help=False,
+        description="List all the bookings that you offer")
+
+    return parser
+
